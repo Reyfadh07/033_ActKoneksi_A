43976d709ee30dcf7cb703eac6f3be9504f5d39c
Reyfadh07 <raihan.farmasi@gmail.com>
2022-03-16 15:46:14 +0700

    Add project files.

---
+++ create_table/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Create_Tabel
+{
+    class Program
+    {
+        public void CreateTable()
+        {
+            SqlConnection con = null;
+            try
+            {
+                con = new SqlConnection("data source=LAPTOP-GK6AO77T;database=ProdiTI; Integrated Security = True");
+                con.Open();
+
+
+            }
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
