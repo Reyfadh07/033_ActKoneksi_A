8071023dc34fcbda419a5993580f550b717f1d7f
Reyfadh07 <raihan.farmasi@gmail.com>
2022-03-16 15:53:18 +0700

    membuat tabel

--- create_table/Program.cs
+++ create_table/Program.cs
@@ -20,6 +20,12 @@ namespace Create_Tabel
                 SqlCommand cm = new SqlCommand("create table Mahasiswa_Coba (NIM char(12) not null primary key, Nama varchar(50), Alamat varchar(255), Jenis_Kelamin char(1)", con);
                 cm.ExecuteNonQuery();
 
+                Console.WriteLine("Tabel sukses dibuat!");
+                Console.ReadKey();
+
+
+            }catch (Exception e)
+            {
 
             }
         }
