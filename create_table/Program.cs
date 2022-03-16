4d4cbe9a1134725211b1b2ac1943de7ef2e27dff
Reyfadh07 <raihan.farmasi@gmail.com>
2022-03-16 16:17:52 +0700

    benerin typo

--- create_table/Program.cs
+++ create_table/Program.cs
@@ -14,7 +14,7 @@ namespace Create_Tabel
             SqlConnection con = null;
             try
             {
-                con = new SqlConnection("data source=LAPTOP-GK6AO77T;database=ProdiTI;Integrated Security = True");
+                con = new SqlConnection("data source=LAPTOP-GK6AO77T;database=ProdiTI;Integrated Security = TRUE");
                 con.Open();
 
                 SqlCommand cm = new SqlCommand("Create table Mahasiswa_Coba (NIM char(12) not null primary key, Nama varchar(50), Alamat varchar(255), Jenis_Kelamin char(1)", con);
